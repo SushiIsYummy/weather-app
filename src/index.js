b97cfcbee1314f2463120ca3e6a0ff9f87c89f38
Alvin Lau <bobthebuilder500000@gmail.com>
2023-10-08 23:53:04 -0400

    Add styling to 7-day forecast section

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import './styles/forecast-weather-display.css';
 import './styles/today-hourly-weather.css';
 import './styles/additional-weather-info.css';
 import './styles/layout.css';
-// import './images/weather-background.jpg';
-
 import addEventListeners from './js/eventHandlers';
 
 addEventListeners();
